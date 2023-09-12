1cc476d570352dda599d97e6312ffa6313f3327a
daffagrahito <muhammad.daffa23@ui.ac.id>
2023-09-13 06:17:24 +0700

    Update Django variable

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'app_name': "daffagrahito's inventory master",
+        'app_name': "daffagrahito's Inventory Master",
         'name': 'Muhammad Daffa Grahito Triharsanto',
         'class': 'PBP A'
     }
