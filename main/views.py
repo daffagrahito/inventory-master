6e837ce248ca89f6ce49fe0d167571a1d366f0c7
daffagrahito <muhammad.daffa23@ui.ac.id>
2023-09-12 15:28:01 +0700

    Code adjustment

--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
+        'app_name': "daffagrahito's inventory master",
         'name': 'Muhammad Daffa Grahito Triharsanto',
         'class': 'PBP A'
     }
