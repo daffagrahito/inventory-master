213a93a2f09efca0010ed307ec09b7016a4d609a
daffagrahito <muhammad.daffa23@ui.ac.id>
2023-09-13 07:05:44 +0700

    Update tests

--- main/tests.py
+++ main/tests.py
@@ -8,3 +8,12 @@ class mainTest(TestCase):
     def test_main_using_main_template(self):
         response = Client().get('/main/')
         self.assertTemplateUsed(response, 'main.html')
+
+    def test_name_and_class_are_set_correctly_in_main_template(self):
+        name = "Muhammad Daffa Grahito Triharsanto"
+        class_name = "PBP A"
+
+        response = Client().get('/main/', {'name': name, 'class': class_name})
+
+        self.assertContains(response, name)
+        self.assertContains(response, class_name)
